b2f7583b43019597432e62f59595c15ba07e7ffd
Fegartyx <antartyx@gmail.com>
2021-07-01 19:52:31 +0700

    Add some icon

--- app/src/main/java/com/example/dokulocker/MainActivity2.java
+++ app/src/main/java/com/example/dokulocker/MainActivity2.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
